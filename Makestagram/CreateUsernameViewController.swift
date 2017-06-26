e8ab074b8a6dcf0468ec513b16f3d7f755e9d7a1
Howard Wang <howardwang00@users.noreply.github.com>
2017-06-26 13:34:29 -0700

    Created UserService Layer

--- Makestagram/CreateUsernameViewController.swift
+++ Makestagram/CreateUsernameViewController.swift
@@ -25,20 +25,10 @@ class CreateUsernameViewController: UIViewController {
             let username = usernameTextField.text,
             !username.isEmpty else { return }
         
-        let userAttrs = ["username": username]
-        let ref = Database.database().reference().child("users").child(firUser.uid)
-        ref.setValue(userAttrs) { (error, ref) in
-            if let error = error {
-                assertionFailure(error.localizedDescription)
-                return
-            }
-            
-            ref.observeSingleEvent(of: .value, with: { (snapshot) in
-                let user = User(snapshot: snapshot)
-            })
+        UserService.create(firUser, username: username) { (user) in
+            guard let user = user else { return }
+            print("Created new user: \(user.username)")
         }
-        
-        
     }
     
 }
