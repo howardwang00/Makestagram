623f7dedee176e11bd6dd71c92146059d66fd31b
Howard Wang <howardwang00@users.noreply.github.com>
2017-06-26 13:58:59 -0700

    Shifted to Main Storyboard after Login

--- Makestagram/CreateUsernameViewController.swift
+++ Makestagram/CreateUsernameViewController.swift
@@ -28,6 +28,12 @@ class CreateUsernameViewController: UIViewController {
         UserService.create(firUser, username: username) { (user) in
             guard let user = user else { return }
             print("Created new user: \(user.username)")
+            
+            let storyboard = UIStoryboard(name: "Main", bundle: .main)
+            if let initialViewController = storyboard.instantiateInitialViewController() {
+                self.view.window?.rootViewController = initialViewController
+                self.view.window?.makeKeyAndVisible()
+            }
         }
     }
     
